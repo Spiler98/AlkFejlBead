59ceff1a6189097ff5612750e170ad03225ae459
Kovács Tamás <jp56x5@inf.elte.hu>
2019-12-18 13:58:45 +0100

    Add mive page

--- Frontend/cinema/src/app/app.module.ts
+++ Frontend/cinema/src/app/app.module.ts
@@ -1,16 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/cinema/src/app/shared/movie.ts
@@ -0,0 +1,12 @@
+export class Movie {
+    id: number;
+    title: string;
+    director: string;
+    synopsis: string;
+    category: string;
+    pegiRating: string;
+    duration: number;
+    posterPath: string;
+    uploadDate: string;
+    showTimes: string[];
+}
